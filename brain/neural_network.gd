8428db6a0be6ddf2448fc16731f47d503cf04be5
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-04 15:14:07 +0700

    create Neural Network

---
+++ brain/neural_network.gd
@@ -0,0 +1,22 @@
+extends Object
+class_name NeuralNetwork
+
+var levels = []
+
+
+func _init(neuron_counts: Array):
+	for i in len(neuron_counts) - 1:
+		var l = Level.new(neuron_counts[i], neuron_counts[i + 1])
+		levels.push_back(l)
+
+
+static func feed_forward(given_inputs, network: NeuralNetwork):
+	var outputs
+
+	for i in len(network.levels):
+		if i == 0:
+			outputs = Level.feed_forward(given_inputs, network.levels[0])
+		else:
+			outputs = Level.feed_forward(outputs, network.levels[i])
+
+	return outputs
