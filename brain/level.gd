bd64c342e55b01b2746c6158ab52520ddc83e356
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-05 22:39:15 +0700

    change population loc, using new mutate algorithm

--- brain/level.gd
+++ brain/level.gd
@@ -61,8 +61,12 @@ func mutate(rate):
 	for i in len(inputs):
 		for j in len(outputs):
 			if randf() < rate:
-				weights[i][j] = rand_range(-1, 1)
+				# weights[i][j] = rand_range(-1, 1)
+				# weights[i][j] *= rand_range(1-rate, 1+rate)
+				weights[i][j] = lerp(weights[i][j], rand_range(-1, 1), rate)
 
 	for i in len(biases):
 		if randf() < rate:
-			biases[i] = rand_range(-1, 1)
+			# biases[i] = rand_range(-1, 1)
+			# biases[i] *= rand_range(1-rate, 1+rate)
+			biases[i] = lerp(biases[i], rand_range(-1, 1), rate)
---
+++ world/population.gd
@@ -0,0 +1,43 @@
+extends Object
+class_name Population
+
+var _Scene: PackedScene
+
+var scenes = []
+var mutation_rate = 0.1
+var generation = 0
+
+
+func _init(object, population_size):
+	_Scene = object
+	scenes.resize(population_size)
+	for i in len(scenes):
+		var scene = _Scene.instance()
+		scenes[i] = scene
+
+
+func selection():
+	# var pool = []
+	var best_score = 0
+	var best_scene
+	for scene in scenes:
+		# for s in scene.score:
+		# 	pool.append(scene)
+		if scene.score > best_score:
+			best_score = scene.score
+			best_scene = scene
+
+	for i in len(scenes):
+		# var r = randi() % len(pool)
+		# var scene = pool[r]
+		# var new_brain = scene.brain.copy()
+		var new_brain = best_scene.brain.copy()
+		new_brain.mutate(mutation_rate)
+		scenes[i].brain = new_brain
+
+
+func reincarnate():
+	selection()
+
+	for scene in scenes:
+		scene.reincarnate()
